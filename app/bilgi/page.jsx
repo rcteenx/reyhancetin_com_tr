4aa302823183c5342d036195d46d339b108ee326
“Reyhan <rcteenx@gmail.com>
2024-08-28 21:37:07 +0300

    update reyhan pages

--- app/bilgi/page.jsx
+++ app/bilgi/page.jsx
@@ -57,6 +57,7 @@ export default function StaticPage() {
       </BlueContent>
       <SubMenu />
       <SocialMedia />
+      
     </>
   );
 }
--- app/biz/kocluk/page.jsx
+++ app/biz/kocluk/page.jsx
@@ -1,13 +1,11 @@
 import PageInfo from "@/components/templates/pageInfo";
 import BlueContent from "@/components/templates/blueContent";
-import SubMenu from "@/components/sections/common/sub-menu";
 import SocialMedia from "@/components/sections/common/z-social-media";
 
 export default function StaticPage() {
   return (
     <>
-      <PageInfo id="1" />
-      {/* <SubMenu /> */}
+      <PageInfo id="13" />
       <SocialMedia />
     </>
   );
